830f039a4f90815f10ca83387b67c7d9654df3a5
Ultragodop <167634718+Ultragodop@users.noreply.github.com>
2025-05-23 04:39:35 -0300

    Commit ccccsssss

---
+++ src/main/java/com/project/minimercado/controllers/auth/AuthController.java
@@ -0,0 +1,7 @@
+package com.project.minimercado.controllers.auth;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+}
---
+++ src/main/java/com/project/minimercado/controllers/bussines/InventarioController.java
@@ -0,0 +1,7 @@
+package com.project.minimercado.controllers.bussines;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InventarioController {
+}
