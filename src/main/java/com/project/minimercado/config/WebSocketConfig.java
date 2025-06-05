e6b68a7689f8e1d405078d30df928a4562752281
santi <ultragodop@gmail.com>
2025-06-05 15:06:14 -0300

    En vez de instanciar un nuevo chatwebsockethandler, se inyecta por constructor, y se usa, optimizacion:)

--- src/main/java/com/project/minimercado/config/WebSocketConfig.java
+++ src/main/java/com/project/minimercado/config/WebSocketConfig.java
@@ -8,7 +8,10 @@ import org.springframework.web.socket.config.annotation.*;
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
-
+    private final ChatWebSocketHandler chatWebSocketHandler;
+    public WebSocketConfig(){
+    this.chatWebSocketHandler = new ChatWebSocketHandler();
+    }// en vez de crar una nueva instancia de chatwebsockethandler en cada peticion, se inyecta chatwebsockethandler en websocketconfig para despues usarla en cada peticion
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new ChatWebSocketHandler(), "/chat").setAllowedOrigins("*");
