dd99bf060c607fb077066b17908a0d49fe881b24
Ultragodop <167634718+Ultragodop@users.noreply.github.com>
2025-08-12 17:24:42 -0300

    appapasasaassaasasa

--- src/main/java/com/project/minimercado/dto/bussines/Ventas/VentaDTO.java
+++ src/main/java/com/project/minimercado/dto/bussines/Ventas/VentaDTO.java
@@ -1,12 +1,15 @@
 package com.project.minimercado.dto.bussines.Ventas;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 
 public interface VentaDTO {
         Integer getIdVenta();
         String getNombre();
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
         Instant getFecha();
         String getTipoPago();
         String getEstado();
