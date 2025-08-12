d186e116148e5fbc891b4cfe8a32de43cd28d7ce
Ultragodop <167634718+Ultragodop@users.noreply.github.com>
2025-08-12 00:40:08 -0300

    appapasasaassaasasa

--- src/main/java/com/project/minimercado/config/WebConfig.java
+++ src/main/java/com/project/minimercado/config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,7 +18,7 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
+    /**@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns(
@@ -33,7 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .maxAge(3600);
     }
-
+**/
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
