97b2d2a64afd35039e7cf6980976969509586fd9
Ultragodop <167634718+Ultragodop@users.noreply.github.com>
2025-08-11 21:47:16 -0300

    niggssss

--- src/main/java/com/project/minimercado/config/WebConfig.java
+++ src/main/java/com/project/minimercado/config/WebConfig.java
@@ -24,7 +24,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns(
                         "http://localhost:[0-9]+",
                         "http://127.0.0.1:[0-9]+",
-                        "http://localhost:5173",
                         "null"
 
                 )
